565500c6f7173dae2d16339f999e33332165fc17
lordev <lorenzo.sallons93@gmail.com>
2024-08-14 21:47:15 +0200

    style change infinite slider respinviness and removed unused blog link menu

--- src/components/ui/sliders/InfiniteSlider.tsx
+++ src/components/ui/sliders/InfiniteSlider.tsx
@@ -6,7 +6,7 @@ export default function InfiniteSlider() {
 			gradient={false}
 			speed={40}
 			autoFill
-			className=" py-1 overflow-hidden h-full min-[280px]:block font-krona-one text-2xl text-primary uppercase tracking-wide bg-white "
+			className=" py-1 overflow-hidden h-full min-[280px]:block font-krona-one text-normal  sm:text-lg md:text-2xl text-primary uppercase tracking-wide bg-white "
 		>
 			<span className="mx-4">
 				Get your tickets before it{"'"}s too late -{' '}
--- src/lib/data/links.ts
+++ src/lib/data/links.ts
@@ -3,7 +3,6 @@ export const menuLinks = [
 	{ text: 'Artists', url: '/artists' },
 	{ text: 'Contact', url: '/contact' },
 	{ text: 'Menu', url: '/menu' },
-	{ text: 'Blog', url: '/blog' },
 ];
 
 export const footerLinks = [
